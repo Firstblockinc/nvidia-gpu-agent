5d30da2be69ab8f369b4705249f30bcafea777f6
jayslachance <ja_sonlachance@hotmail.com>
2024-09-22 11:05:39 -0400

    changed gpu Class to simple function

---
+++ app/gpu_manager.py
@@ -0,0 +1,23 @@
+import pynvml
+
+def get_gpu_info():
+    pynvml.nvmlInit()
+    try:
+        device_count = pynvml.nvmlDeviceGetCount()
+        gpu_data = []
+
+        for i in range(device_count):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+            info = {
+                'id': i,
+                'name': pynvml.nvmlDeviceGetName(handle).decode('utf-8'),
+                'temperature': pynvml.nvmlDeviceGetTemperature(handle, pynvml.NVML_TEMPERATURE_GPU),
+                'utilization': pynvml.nvmlDeviceGetUtilizationRates(handle).gpu,
+                'vram_used': pynvml.nvmlDeviceGetMemoryInfo(handle),
+                'consumption': pynvml.nvmlDeviceGetPowerUsage(handle)
+            }
+            gpu_data.append(info)
+
+        return gpu_data
+    finally:
+        pynvml.nvmlShutdown()
